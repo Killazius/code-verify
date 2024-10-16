47d692f601973b2611262119e05529c3dabca947
Максим Мигунов <89807644+Killazius@users.noreply.github.com>
2024-10-16 20:45:39 +0300

    Update user_file.go

--- internal/compilation/user_file.go
+++ internal/compilation/user_file.go
@@ -11,10 +11,7 @@ func MakeFile(Link string, format string) error {
 	endpoint := "--endpoint-url=https://s3.ru-1.storage.selcloud.ru"
 	cmd := exec.Command("aws", "s3", "cp")
 	cmd.Args = append(cmd.Args, endpoint, Link, fmt.Sprintf("user.%s", format))
-	start := time.Now()
 	err := cmd.Run()
-	end := time.Now()
-	fmt.Println(start, end)
 	if err != nil {
 		return fmt.Errorf("ошибка выполнения команды: %v", err)
 	}

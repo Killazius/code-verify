490edc067e0fcafbab3e51ace41fd4e2205efadb
Killazius <migunovmaksim44@gmail.com>
2025-01-30 19:48:37 +0300

    5 seconds context to tests

--- internal/compilation/test/test.go
+++ internal/compilation/test/test.go
@@ -2,13 +2,15 @@ package test
 
 import (
 	"compile-server/internal/compilation"
+	"compile-server/internal/handlers/ws/utils"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 type testCase struct {
@@ -21,12 +23,40 @@ type testCases []testCase
 func Run(command string, args ...string) (string, error) {
 	path := "src/1-sum/"
 	var tests testCases
+	err := readTestCases(path, &tests)
+	if err != nil {
+		return "", err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	for i, test := range tests {
+		cmd := exec.CommandContext(ctx, command, args...)
+		cmd.Stdin = strings.NewReader(test.Input[0] + "\n" + test.Input[1] + "\n")
+		output, errCmd := cmd.CombinedOutput()
+		select {
+		case <-ctx.Done():
+			return utils.Timeout, ctx.Err()
+		default:
+			if errCmd != nil {
+				return "", fmt.Errorf("command execution failed: %w", errCmd)
+			}
+			result := strings.TrimSpace(string(output))
+			if result != test.Answer {
+				return fmt.Sprintf("Test case #%d failed. Expected: %s, Got: %s", i+1, test.Answer, result), nil
+
+			}
+		}
+	}
+	return "success", nil
+}
+
+func readTestCases(path string, tests *testCases) error {
 	file, err := os.Open(fmt.Sprintf("%v%v", path, compilation.TestFile))
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 	defer func(file *os.File) {
-		err := file.Close()
+		err = file.Close()
 		if err != nil {
 			return
 		}
@@ -34,26 +64,12 @@ func Run(command string, args ...string) (string, error) {
 
 	byteValue, err := io.ReadAll(file)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	err = json.Unmarshal(byteValue, &tests)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-
-	for i, test := range tests {
-		cmd := exec.Command(command, args...)
-		cmd.Stdin = strings.NewReader(test.Input[0] + "\n" + test.Input[1] + "\n")
-
-		output, errCmd := cmd.CombinedOutput()
-		result := strings.TrimSpace(string(output))
-		if errCmd != nil {
-			return "", errCmd
-		}
-		if result != test.Answer {
-			return fmt.Sprintf("Test case #%d failed.\n", i+1), nil
-		}
-	}
-	return "success", nil
+	return nil
 }

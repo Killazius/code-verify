58e5f9fd79fad4bf888a71cdf066079cf1be4dbb
Максим Мигунов <migunovmaksim44@gmail.com>
2025-02-20 20:11:40 +0300

    delete -race flag in integration tests. delete some comment-line

--- internal/middleware/customlogger/customlogger.go
+++ internal/middleware/customlogger/customlogger.go
@@ -20,7 +20,6 @@ func New(log *slog.Logger) func(next http.Handler) http.Handler {
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
 				slog.String("remote_addr", r.RemoteAddr),
-				//slog.String("user_agent", r.UserAgent()),
 				slog.String("request_id", middleware.GetReqID(r.Context())),
 			)
 			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
@@ -28,8 +27,6 @@ func New(log *slog.Logger) func(next http.Handler) http.Handler {
 			t1 := time.Now()
 			defer func() {
 				entry.Info("request completed",
-					//slog.Int("status", ww.Status()),
-					//slog.Int("bytes", ww.BytesWritten()),
 					slog.String("duration", time.Since(t1).String()),
 				)
 			}()
